55f932881c54747f0c13d6abf4a10df587842721
maccwinter <matthew.winter1@louisiana.edu>
2019-09-09 11:20:08 -0500

    added comment

--- Fishstuffs.R
+++ Fishstuffs.R
@@ -1,1 +1,9 @@
 #fishstuff
+NROW(`fish_data.(1)`)
+#There are 27603 rows 
+NCOL(`fish_data.(1)`)
+#There is 1 column 
+c(`fish_data.(1)`)
+class(`fish_data.(1)`)
+str(`fish_data.(1)`)
+#try now with properly-loaded fish data
