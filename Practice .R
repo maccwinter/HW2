edbb5534c635307e79fc2f1bd865dae44dd10f52
maccwinter <matthew.winter1@louisiana.edu>
2019-09-16 11:35:23 -0500

    practiced some

--- Practice .R
+++ Practice .R
@@ -3,3 +3,39 @@
 die <- c(1,2,3,4,5,6)
 die
 is.vector(die)
+object <- c(1,2,3,4,5,6)
+object
+die <- 1:6
+die
+ls()
+sample(die, size=2)
+sample(die, size=3)
+mean(object)
+round(mean(die))
+sample(1:4, size=2)
+args(sample)
+sample(die, size=2, replace = TRUE)
+k <- sample(die, size=2, replace = TRUE)
+k
+k
+k
+sum(k)
+roll(k)
+roll()
+m <- function(x,y) {sample(x*1:20/y)}
+m(1,2)
+'%tree%' <- function(a, b) {2*a + 3*b/sample(5:23, size = 1, replace = TRUE)}
+1%tree%3
+2%tree%5
+list("hey", 3, 1, 6)
+list[2]
+[2]
+[2]list
+animals <- c("cat", "pig", "turtle", "fish")
+animals[2]
+animals[4]
+v <- 1:100
+v[50]
+ls()
+class(v)
+head(v)
