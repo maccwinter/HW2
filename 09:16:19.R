f4bf92aa8671c3feac767003a055fab2af775742
maccwinter <matthew.winter1@louisiana.edu>
2019-09-23 15:48:30 -0500

    Did stiff for class 9/23

--- 09:16:19.R
+++ 09:16:19.R
@@ -1,7 +1,7 @@
 #Class September 16 
 list.files(pattern=".txt")
 #This finds the file that is a text file. 
-#I found that long-ass ISI37281080643820`48930217483902 file`
+#I found that long-ass ISIIS20140529124237281080643820473821907458390625478923489210778492018930217483902.txt file`
 
 #To get the file read in. I will name it "o" 
 o <- read.table(file='ISIIS201405291242.txt', sep = "\t", skip=10, header=TRUE, fileEncoding = "ISO-8859-1", stringsAsFactors = FALSE, quote = "\"", check.names = FALSE, encoding="UTF-8", na.strings="9999.99")
@@ -65,7 +65,55 @@ o$min <- as.numeric(str_sub(o$Time,4,5))
 o$min
 head(o)
 #now there is a minute column 
+library(stringr)
+library("stringr")
+date
+date <-date[2]
+date
 
+dd <- (str_sub(date, 4,5))
+dd
+dd <- as.numeric(dd)
+dd
+o$Time
+str(o$Time)
+o$hour <- as.numeric(str_sub(o$Time,1,2))
+head(o)
+o$minute <- as.numeric(str_sub(o$Time,4,5))
+head(o)
+str(o$Time)
+o$second <- as.numeric(str_sub(o$Time, 7,11))
+head(o)
+str(o$Time)
+head(o)
+date <- str_c(mm,dd,yy, sep="/")
+date
+unique(yy)
+unique(mm)
+unique(dd)
+
+o$date <-date
+head(o)
+
+
+o$time <- str_c(o$hour, o$minute, o$second, sep=":")
+
+options("digits.secs"=3)
+o$DateTime <- str_c(o$date, o$time, sep=" ")
+head(o$DateTime)
+
+
+o$DateTime <- as.POSIXct(strptime(x=o$DateTime, format = "%m/%d/%y %H:%M:%S", tz="America/New_York"))
+
+head(o)
+str(head(o))
+str(head(o$DateTime))
+install.packages("openxlsx")
+library("openxlsx")
+#This is for opening excel files, which we decided not to do. We saved the excel file as a CV file. 
+
+au <-read.table(file="Aurelia_SEAMAP_2012-2018_30minCell.csv", sep=",", header=T, stringsAsFactors = F)
+head(au)
 
 
 
