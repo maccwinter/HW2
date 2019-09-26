180be0d70de8bb3e7e5ad9c6a11b3336aa2bc7a1
maccwinter <matthew.winter1@louisiana.edu>
2019-09-25 21:02:20 -0500

    commitment LOL

--- Lab 4.R
+++ Lab 4.R
@@ -14,5 +14,27 @@ d5 <- read_xlsx(path="Aurelia_SEAMAP_2012-2018_30minCell.xlsx", sheet =1, col_na
 d6 <- load("aurelia_15minCell_statareas.Rdata")
 head(d6)
 
-d6 <- read_csv(file="aurelia_15minCell_statareas.txt")
+d7 <- read_csv(file="aurelia_15minCell_statareas.txt")
 head(d6)
+d8 <- read_csv(file="ENVREC.csv")
+head(d7)
+d9 <- read_csv(file = "STAREC_rev20190402.csv")
+d10 <- read_csv(file = "INGEST_DATA.csv")
+head(d9)
+d11 <- read_csv("NEWBIOCODESBIG.csv")
+d12 <- read_csv("SHRREC.csv")
+d13 <- read_csv("CTDCASTREC.csv")
+d14 <- read_csv("INVREC.csv")
+d15 <- read_csv("ISTREC.csv")
+d16 <- read_csv("VESSELS.csv")
+d17 <- read_csv("CTDREC.csv")
+d18 <- read_csv("BGSREC.csv")
+d19 <- read_csv("CRUISES.csv")
+d20 <- read_csv("GLFREC.csv")
+d21 <- read_csv("lg_camera_class_groupings_20170113_phylo_orderd.csv")
+d22 <- read_xlsx("transect_towtime.xlsx")
+d23 <- read_xlsx("NameTranslator_table20140330.xlsx")
+#DONE
+
+
+
