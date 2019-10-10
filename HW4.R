e5580c1c7c3badd040840ea0b025461068535582
maccwinter <matthew.winter1@louisiana.edu>
2019-10-09 22:04:59 -0500

    making it look pretty

--- HW4.R
+++ HW4.R
@@ -4,55 +4,52 @@ load("fish_data (1).Rdata")
 library(tidyverse)
 
 #Question 1----
-p.d.m <- tapply(X = f$parcel.density.m3, INDEX = (f$transect.id), FUN=mean)
-p.d.m
+parceldensity.m <- tapply(X = f$parcel.density.m3, INDEX = (f$transect.id), FUN=mean)
+head(parceldensity.m)
 #Question 2 ----
-pdmd <- as.data.frame(p.d.m)
-pdmd
-nrow(pdmd)
-pdmd$seq <- 1:39
-#there are 39
+parceldensity.m.d <- as.data.frame(parceldensity.m)
+head(parceldensity.m.d)
+nrow(parceldensity.m.d)
+parceldensity.m.d$seq <- 1:39
 #Question 3----
-colnames(pdmd) <-  c("density.mean", "seq")
-head(pdmd)
+colnames(parceldensity.m.d) <-  c("density.mean", "seq")
+head(parceldensity.m.d)
 #Question 4----
-transect <- rownames(pdmd)
-transect
-#Question 5----
-p.d.st <- tapply(X = f$parcel.density.m3, INDEX =(f$transect.id), FUN=sd)
-pdstd <-as.data.frame(p.d.st)
-colnames(pdstd) <-  "density.st.dev"
-transect <- rownames(pdstd)
-transect
-nrow(pdstd)
-pdstd$seq <-1:39
+transect <- rownames(parceldensity.m.d)
+head(transect)
 transect <- as.data.frame(transect)
 head(transect)
-#There are 39 rows 
 transect$seq <- 1:39
+#Question 5----
+parceldensity.sd <- tapply(X = f$parcel.density.m3, INDEX =(f$transect.id), FUN=sd)
+parceldensity.sd.d <-as.data.frame(parceldensity.sd)
+colnames(parceldensity.sd.d) <-  "density.st.dev"
+transect <- rownames(parceldensity.sd.d)
+nrow(parceldensity.sd.d)
+parceldensity.sd.d$seq <-1:39
 #Question 6 ----
-head(pdstd)
-head(pdmd)
-parcel.density <- merge(transect, c(pdstd,pdmd), by="seq")
+head(parceldensity.sd.d)
+head(parceldensity.m.d)
+parcel.density <- merge(transect, c(parceldensity.sd.d,parceldensity.m.d), by="seq")
 head(parcel.density)
 parcel.density[,c("seq","seq.1")] <- NULL
 head(parcel.density)
-#There are 1521 
 head(parcel.density)
 colnames(parcel.density) <- c("transect", "standard.dev", "mean")
 head(parcel.density)
 #Question 7 ----
-p.d.c <- tapply(X = f$parcel.density.m3, INDEX = list(f$transect.id), FUN=length)
-p.d.c
-pdcd <- as.data.frame(p.d.c)
-head(pdcd)
-colnames(pdcd) <- "count"
-head(pdcd)
-nrow(pdcd)
-pdcd$seq <- 1:39
+parceldensity.c <- tapply(X = f$parcel.density.m3, INDEX = list(f$transect.id), FUN=length)
+head(parceldensity.c)
+parceldensity.c.d <- as.data.frame(parceldensity.c)
+head(parceldensity.c.d)
+colnames(parceldensity.c.d) <- "count"
+head(parceldensity.c.d)
+nrow(parceldensity.c.d)
+parceldensity.c.d$seq <- 1:39
+head(parceldensity.c.d)
 #Question 8 ----
 parcel.density$seq <- 1:39
-newdataframe1 <- merge(parcel.density, pdcd, by="seq")
+newdataframe1 <- merge(parcel.density, parceldensity.c.d, by="seq")
 head(newdataframe1)
 nrow(newdataframe1)
 newdataframe1[,"seq"] <- NULL
@@ -61,53 +58,51 @@ colnames(newdataframe1) <- c("transect.id","density.stdv", "density.mean", "obs.
 head(newdataframe1)
 #Question 9 ----
 
-dens.mean2 <- f %>% group_by(transect.id) %>% summarise(pdens2 = mean(parcel.density.m3))
-head(dens.mean2)
+parceldensity.m.2 <- f %>% group_by(transect.id) %>% summarise(density = mean(parcel.density.m3))
+head(parceldensity.m.2)
 #Question 10 ---- 
 
-dmean2 <- as.data.frame(dens.mean2)
-head(dmean2)
+parceldensity.m.2.d <- as.data.frame(parceldensity.m.2)
+head(parceldensity.m.2.d)
 #Question 11 ---- 
-colnames(dmean2) <- c("transect.id","parcel.density.mean")
-head(dmean2)
-nrow(dmean2)
-dmean2$seq <- 1:39
+colnames(parceldensity.m.2.d) <- c("transect","mean")
+head(parceldensity.m.2.d)
+nrow(parceldensity.m.2.d)
+parceldensity.m.2.d$seq <- 1:39
 #Question 12 
 
-transect2 <- dmean2[,1]
+transect2 <- parceldensity.m.2.d[,1]
 head(transect2)
 transect2.0 <- as.data.frame(transect2)
 head(transect2.0)
 transect2.0$seq <- 1:39
 head(transect2.0)
 #Question 13 ---- 
-densit.sd2 <- tapply(X=f$parcel.density.m3, INDEX = f$transect.id, FUN =sd)
-head(densit.sd2)
-dsd2 <- as.data.frame(densit.sd2)
-head(dsd2)
-ncol(dsd2)
-dsd2$seq <- 1:39
-head(dsd2)
-dsd2$"NA" <- NULL
-head(dsd2)
-colnames(dsd2) <- c("parcel.density.std", "seq")
-head(dsd2)
-transect3 <- rownames(dsd2)
+parceldensity.sd.2 <- tapply(X=f$parcel.density.m3, INDEX = f$transect.id, FUN =sd)
+head(parceldensity.sd.2)
+parceldensity.sd.d.2 <- as.data.frame(parceldensity.sd.2)
+head(parceldensity.sd.d.2)
+ncol(parceldensity.sd.d.2)
+parceldensity.sd.d.2$seq <- 1:39
+head(parceldensity.sd.d.2)
+colnames(parceldensity.sd.d.2) <- c("st.dev", "seq")
+head(parceldensity.sd.d.2)
+transect3 <- rownames(parceldensity.sd.d.2)
 head(transect3)
 #Question 14 ---- 
-dataframe3 <- merge(dmean2,dsd2, by="seq")
+dataframe3 <- merge(parceldensity.m.2.d,parceldensity.sd.d.2, by="seq")
 head(dataframe3)
 dataframe4 <- right_join(x=transect2.0, y=dataframe3, by = "seq")
 head(dataframe4)
 dataframe4[,"seq"] <- NULL
 head(dataframe4)
 #Question 15 ---- 
-pd3 <- tapply(X=f$parcel.density.m3, INDEX = f$transect.id, FUN = length)
-head(pd3)
-pd3. <- as.data.frame(pd3)
-head(pd3.)
-pd3.$seq <- 1:39
-head(pd3.)
+parceldensity.c.2<- tapply(X=f$parcel.density.m3, INDEX = f$transect.id, FUN = length)
+head(parceldensity.m.2)
+parceldensity.m.2.d <- as.data.frame(parceldensity.m.2)
+head(parceldensity.m.2.d)
+parceldensity.m.2.d$seq <- 1:39
+head(parceldensity.m.2.d)
 colnames(pd3.) <- c("count", "seq")
 head(pd3.)
 
